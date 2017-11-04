fc2437b06bac91c2754cf32cafb72b9f56c76729
Shahab Arabshahi <shahab.arabshahi@jpl.nasa.gov>
2017-11-03 17:01:10 -0700

    solved level 5 and update README.md

--- level_5/dodge_the_lasers/solution.py
+++ level_5/dodge_the_lasers/solution.py
@@ -1,2 +1,18 @@
+from decimal import Decimal, getcontext
+
+
 def answer(str_n):
-    # your code here
+    # sum of a Beatty sequence with irrational number sqrt(2) and time
+    # complexity O(logN)
+    def beattysum(n):
+        getcontext().prec = 101
+        np = long((Decimal(2).sqrt() - Decimal(1))*Decimal(n))
+
+        if n == 1:
+            return n
+        if n < 1:
+            return 0
+        return n*np + n*(n + 1)/2 - np*(np + 1)/2 - beattysum(np)
+
+    n = long(str_n)
+    return str(beattysum(n))
