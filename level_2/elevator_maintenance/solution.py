9ce52547aacba54b76c7a22874c679bb503190af
Shahab Arabshahi <shahab.arabshahi@jpl.nasa.gov>
2017-11-14 13:11:26 -0800

    Added solution

--- level_2/elevator_maintenance/solution.py
+++ level_2/elevator_maintenance/solution.py
@@ -1,2 +1,5 @@
 def answer(l):
-    return
+    l_int = [[int(s) for s in v.split(".")] for v in l]
+    l_sorted = sorted(l_int)
+    l_str = [[str(i) for i in v] for v in l_sorted]
+    return [".".join(v) for v in l_str]
